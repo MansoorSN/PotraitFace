50a3044e8bf1718f7f705929a085f183acfc3118
MansoorSN <48001931+MansoorSN@users.noreply.github.com>
2024-08-01 17:49:42 -0400

    Update deepvidface.py

--- deepvidface.py
+++ deepvidface.py
@@ -66,89 +66,7 @@ def process_video(upload_file):
 
     if upload_file is not None:
     
-    class PotraitFace:
-    def __init__(self, img_array):
-        self.image_path=None
-        self.image_array=img_array
-        self.image=Image.fromarray(self.image_array)
-        self.dfs=None
-
-
-    def get_embeddings(self):
-        try:
-            self.dfs = DeepFace.represent(img_path = self.image_array,model_name = 'SFace',detector_backend='yolov8')
-            return self.dfs
-        except:
-            return None
-    @staticmethod
-    def magnified_coordinates(x,y,w,h):
-        X=x-0.25*w if x-0.25*w>=0 else 0
-        Y=y-0.25*h if y-0.25*h>=0 else 0
-        W=w+0.5*w
-        H=h+0.5*h
-        
-        return(X,Y,W,H)
-    
-    def get_potraitface_coordinates(self):
-        #print("getting face coordinates")
-        crop_coordinates=[]
-        if self.dfs==None:
-            return crop_coordinates
-        for i in range(len(self.dfs)):
-            x1=self.dfs[i]['facial_area']['x']
-            y1=self.dfs[i]['facial_area']['y']
-            w=self.dfs[i]['facial_area']['w']
-            h=self.dfs[i]['facial_area']['h']
-
-            X1,Y1,W,H= self.magnified_coordinates(x1,y1,w,h)
-            X2=X1+W
-            Y2=Y1+H
-            crop_coordinates.append((X1,Y1,X2,Y2))
-        print("Done getting face coordinates")    
-        return crop_coordinates
-        
-    def get_potraitfaces(self,coordinates):
-        potraitfaces_list=[]
-
-        #print("getting faces from coordinates")
-        if coordinates==[]:
-            return potraitfaces_list
-
-        for i in range(len(coordinates)):
-            potraitfaces_list.append(self.image.crop(coordinates[i]))
-
-        print(potraitfaces_list)
-        print("Done getting faces from coordinates")
-        return potraitfaces_list
-    
-    
-    def get_potraitfaces_list(self):
-        self.get_embeddings()
-        return self.get_potraitfaces(self.get_potraitface_coordinates())
-
-
-    def get_potraitfaces_dict(self):
-        self.get_embeddings()
-        return self.get_potraitfaces(self.get_potraitface_coordinates())
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+      
         st.write(f"file name : {upload_file.name}")
         st.write("file is mp4")
         # Save the uploaded file to a temporary file
@@ -206,6 +124,74 @@ def process_video(upload_file):
         st.write("done")
         #st.stop()
 if __name__ == '__main__':
+    
+    class PotraitFace:
+        def __init__(self, img_array):
+            self.image_path=None
+            self.image_array=img_array
+            self.image=Image.fromarray(self.image_array)
+            self.dfs=None
+
+
+        def get_embeddings(self):
+            try:
+                self.dfs = DeepFace.represent(img_path = self.image_array,model_name = 'SFace',detector_backend='yolov8')
+                return self.dfs
+            except:
+                return None
+        @staticmethod
+        def magnified_coordinates(x,y,w,h):
+            X=x-0.25*w if x-0.25*w>=0 else 0
+            Y=y-0.25*h if y-0.25*h>=0 else 0
+            W=w+0.5*w
+            H=h+0.5*h
+            
+            return(X,Y,W,H)
+        
+        def get_potraitface_coordinates(self):
+            #print("getting face coordinates")
+            crop_coordinates=[]
+            if self.dfs==None:
+                return crop_coordinates
+            for i in range(len(self.dfs)):
+                x1=self.dfs[i]['facial_area']['x']
+                y1=self.dfs[i]['facial_area']['y']
+                w=self.dfs[i]['facial_area']['w']
+                h=self.dfs[i]['facial_area']['h']
+
+                X1,Y1,W,H= self.magnified_coordinates(x1,y1,w,h)
+                X2=X1+W
+                Y2=Y1+H
+                crop_coordinates.append((X1,Y1,X2,Y2))
+            print("Done getting face coordinates")    
+            return crop_coordinates
+            
+        def get_potraitfaces(self,coordinates):
+            potraitfaces_list=[]
+
+            #print("getting faces from coordinates")
+            if coordinates==[]:
+                return potraitfaces_list
+
+            for i in range(len(coordinates)):
+                potraitfaces_list.append(self.image.crop(coordinates[i]))
+
+            print(potraitfaces_list)
+            print("Done getting faces from coordinates")
+            return potraitfaces_list
+        
+        
+        def get_potraitfaces_list(self):
+            self.get_embeddings()
+            return self.get_potraitfaces(self.get_potraitface_coordinates())
+
+
+        def get_potraitfaces_dict(self):
+            self.get_embeddings()
+            return self.get_potraitfaces(self.get_potraitface_coordinates())
+        
+    
+    
     upload_file = st.file_uploader("Choose a mp4 file", type=['mp4'], accept_multiple_files=False)
     st.write("Please upload a video in mp4 format")
 
